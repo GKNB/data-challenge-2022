2181e57d46abf0c2cd37d6cb17249fafb8cd4637
Bigeng Wang <bw2482@columbia.edu>
2022-08-23 12:34:41 -0700

    Added GAN generator_loss and discriminator_loss computation routine.

--- lib/GAN_class.py
+++ lib/GAN_class.py
@@ -27,7 +27,7 @@ class AWWSM4_SR_GAN:
 		self.is_GAN = is_GAN
 		self.parameters = deepcopy(parameters)
 		
-	def load_data():
+	def load_data(self):
 		parameters_data = self.parameters['data']
 		data_xy_dict = get_data_xy_from_h5(parameters_data['output_folder'], 
 							parameters_data['file_format'], 
@@ -36,6 +36,27 @@ class AWWSM4_SR_GAN:
 		self.data_x = np.array(data_xy_dict['x'])
 		self.data_y = np.array(data_xy_dict['y'])
 
+
+	def split_data(self, test_set_ratio=0.2, val_set_subratio=0.25,rdm_state=42):
+		#First split data into train+validation and test set
+		self.x_train, self.x_test, self.y_train, self.y_test = \
+					train_test_split(self.data_x, self.data_y, 
+												test_size=test_set_ratio, random_state=rdm_state)
+
+		#Next split training again into train and validation
+		self.x_train, self.x_val, self.y_train, self.y_val = \
+					train_test_split(self.x_train, self.y_train, 
+												test_size=val_set_subratio, random_state=rdm_state)
+		print("x_train.shape: {}".format(self.x_train.shape) )
+		print("x_val.shape: {}".format(self.x_val.shape) )
+		print("x_test.shape: {}".format(self.x_test.shape) )
+
+		print("y_train.shape: {}".format(self.y_train.shape) )
+		print("y_val.shape: {}".format(self.y_val.shape) )
+		print("y_test.shape: {}".format(self.y_test.shape) )
+
+		print(np.max(self.x_train), np.max(self.x_val), np.max(self.x_test), np.min(self.x_train), np.min(self.x_val), np.min(self.x_test))
+		print(np.max(self.y_train), np.max(self.y_val), np.max(self.y_test), np.min(self.y_train), np.min(self.y_val), np.min(self.y_test))
 	
 	def load_gen_model(): 
 		return
@@ -48,19 +69,30 @@ class AWWSM4_SR_GAN:
 	def save_disc_model(): #only save weights
 		return
 
-	def compute_gen_loss():
+	def compute_gen_loss(self):
 		if self.is_GAN == True:
 			#content loss + advers loss, return tot_loss, content_loss, adver_loss
-			return
+			x_HR = self.x_HR
+			x_SR = self.x_SR
+			d_SR = self.d_SR
+			alpha_advers = self.paramters['train']['alpha_advers']
+			content_loss = tf.reduce_mean((x_HR - x_SR)**2)
+			g_advers_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=d_SR, labels=tf.ones_like(d_SR)))
+			g_loss = content_loss + alpha_advers * g_advers_loss
+			return g_loss, content_loss, g_advers_loss
 		elif self.is_GAN == False:
+			print("Currently not implemented!")
 			#only content loss
 			return
 		else:
 			print("Error Unknown GAN option!") 
 
 
-	def compute_disc_loss():
-		return
+	def compute_disc_loss(self):
+		d_HR = self.d_HR
+		d_SR = self.d_SR
+		return tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=tf.concat([d_HR, d_SR], axis=0),
+                             labels=tf.concat([tf.ones_like(d_HR), tf.zeros_like(d_SR)], axis=0)))
 
 	def set_working_mode(self, is_GAN):
 		self.is_GAN = is_GAN
