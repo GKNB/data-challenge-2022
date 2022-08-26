c052fe3d09b178c31ddc4c7cd706f99fd54f3dd9
Bigeng Wang <bw2482@columbia.edu>
2022-08-26 14:43:58 -0700

    Debugged steps for the AE fittings.

--- lib/AE_class.py
+++ lib/AE_class.py
@@ -62,6 +62,7 @@ class AWWSM4_HIER_AE:
 		for i_AE in range(self.n_sub_net):
 			n_AE = i_AE + 1	
 			latent_dim_en_i = self.latent_dim_en
+			print("Doing the %dth sub autoencoder." % n_AE)
 			print("Keeping latent dimension size for encoder same: %d" % latent_dim_en_i)
 			latent_dim_de_i = self.latent_dim_de_origin * n_AE
 			print("Latent dimension size for decoder of the %dth AE is: %d = %d * %d" \
@@ -75,34 +76,34 @@ class AWWSM4_HIER_AE:
 			exec(create_encoder_cmd)
 			print_encoder_cmd = "print(%s.summary())" % encoder_name	
 			print("Executing: "+print_encoder_cmd)
-			##exec(print_encoder_cmd)
+			exec(print_encoder_cmd)
 	
 			#create decoder
 			decoder_name = "decoder_%d_sub%d" % (latent_dim_en_i, n_AE)
 			create_decoder_cmd = "%s = decoder(%d)" % \
 									(decoder_name, latent_dim_de_i)
 			print("Executing: "+create_decoder_cmd)
-			##exec(create_decoder_cmd)
+			exec(create_decoder_cmd)
 
 			print_decoder_cmd = "print(%s.summary())" % decoder_name
 			print("Executing: "+print_decoder_cmd)
-			##exec(print_decoder_cmd)
+			exec(print_decoder_cmd)
 
 			#create Autoencoder_hier_X
 			HIER_AE_name_now = "autoencoder_%d_hier_%d" % (latent_dim_en_i, n_AE)
 			if i_AE == 0:
-				init_HIER_AE_cmd = "Autoencoder_hier_%d(%s, %s)" % (latent_dim_en_i, encoder_name, decoder_name) 
+				init_HIER_AE_cmd = "Autoencoder_hier_%d(%s, %s)" % (n_AE, encoder_name, decoder_name) 
 			elif i_AE > 0:
 				init_HIER_AE_cmd = "Autoencoder_hier_%d(%s, %s, %s)" % \
-												(latent_dim_en_i, encoder_name, decoder_name, HIER_AE_name_prev)
+												(n_AE, encoder_name, decoder_name, HIER_AE_name_prev)
 			
 			create_HIER_AE_cmd = "%s = %s" % (HIER_AE_name_now, init_HIER_AE_cmd)
 			print("Executing: " + create_HIER_AE_cmd)	
-			##exec(create_HIER_AE_cmd)
+			exec(create_HIER_AE_cmd)
 			compile_HIER_AE_cmd = "%s.compile(optimizer='adam', loss=losses.MeanSquaredError())" \
 										% HIER_AE_name_now
 			print("Executing: " + compile_HIER_AE_cmd)	
-			##exec(comple_HIER_AE_cmd)
+			exec(compile_HIER_AE_cmd)
 			#steps to do
 			logdir = 'autoencoder_%d_hier_%d/' % (latent_dim_en_i, n_AE) + datetime.now().strftime('%Y%m%d-%H%M%S')
 			tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
@@ -112,20 +113,21 @@ class AWWSM4_HIER_AE:
 			model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_filepath,
                                                                save_weights_only=True,
                                                                save_freq=10*40)
+			print("Preparation done! Do the training now!")
 			#do fit
 			training_history_cmd = "training_history_hier_%d" % n_AE
 			fit_cmd = "autoencoder_%d_hier_%d.fit" % (latent_dim_en_i, n_AE)
 			arguments_cmd_p1 = "(self.X_train, self.X_train,"
 			arguments_cmd_p2 = "batch_size={}, epochs={}, shuffle={},".format(self.batch_size, 
 																		self.epochs, self.shuffle)
-			arguments_cmd_p3 = "validation_data=(self.X_val, self.X_val)"
+			arguments_cmd_p3 = "validation_data=(self.X_val, self.X_val), "
 			arguments_cmd_p4 = "callbacks=[tensorboard_callback, model_checkpoint_callback])"
             
 			training_history_cmd_full = "%s = %s" % \
 										(training_history_cmd, fit_cmd+arguments_cmd_p1+arguments_cmd_p2\
 													+arguments_cmd_p3+arguments_cmd_p4) 
 			print("Executing: " + training_history_cmd_full)
-			##exec(training_history_cmd_full)
+			exec(training_history_cmd_full)
 
 			#test section
 			if i_AE > 0:
