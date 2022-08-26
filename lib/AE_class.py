ccd85fc25e17a57e9a2f8ae2796421320c8e118b
Bigeng Wang <bw2482@columbia.edu>
2022-08-26 12:00:33 -0700

    Debugged the AE_class members

--- lib/AE_class.py
+++ lib/AE_class.py
@@ -26,10 +26,10 @@ class AWWSM4_HIER_AE:
 		self.latent_dim_en = parameters['train']['latent_dim_en'] 
 		self.latent_dim_de_origin = parameters['train']['latent_dim_de_origin']
 		self.log_format = parameters['train']['log_format']
-		self.self.batch_size = parameters['train']['batch_size']
-		self.epoch = parameters['train']['epoch'] 
-		self.shuffle = = parameters['train']['shuffle']
-
+		self.batch_size = parameters['train']['batch_size']
+		self.epochs = parameters['train']['epochs'] 
+		self.shuffle = parameters['train']['shuffle']
+		self.parameters = deepcopy(parameters)
 
 	def load_data(self):
 		parameters_data = self.parameters['data']
@@ -49,7 +49,7 @@ class AWWSM4_HIER_AE:
 
 		#Next split training again into train and validation
 		self.X_train, self.X_val = \
-					train_test_split(self.X_train, self.y_train, 
+					train_test_split(self.X_train, 
 												test_size=val_set_subratio, random_state=rdm_state)
 		print("X_train.shape: {}".format(self.X_train.shape) )
 		print("X_val.shape: {}".format(self.X_val.shape) )
@@ -64,45 +64,45 @@ class AWWSM4_HIER_AE:
 			latent_dim_en_i = self.latent_dim_en
 			print("Keeping latent dimension size for encoder same: %d" % latent_dim_en_i)
 			latent_dim_de_i = self.latent_dim_de_origin * n_AE
-			print("Latent dimension size for decoder of the %dth AE is: %d = %d * %d") \
-					% (n_AE, latent_dim_de_i, self.latent_dim_de_origin, n_AE)
+			print("Latent dimension size for decoder of the %dth AE is: %d = %d * %d" \
+					% (n_AE, latent_dim_de_i, self.latent_dim_de_origin, n_AE) )
 		
 			#create encoder	
 			encoder_name = "encoder_%d_sub%d" % (latent_dim_en_i, n_AE)
 			create_encoder_cmd = "%s = encoder(%d)" % \
 									(encoder_name, latent_dim_en_i)	
 			print("Executing: "+create_encoder_cmd)
-			exec(create_encoder_cmd)
+			##exec(create_encoder_cmd)
 			print_encoder_cmd = "print(%s.summary())" % encoder_name	
 			print("Executing: "+print_encoder_cmd)
-			exec(print_encoder_cmd)
+			##exec(print_encoder_cmd)
 	
 			#create decoder
 			decoder_name = "decoder_%d_sub%d" % (latent_dim_en_i, n_AE)
 			create_decoder_cmd = "%s = decoder(%d)" % \
 									(decoder_name, latent_dim_de_i)
 			print("Executing: "+create_decoder_cmd)
-			exec(create_decoder_cmd)
+			##exec(create_decoder_cmd)
 
 			print_decoder_cmd = "print(%s.summary())" % decoder_name
 			print("Executing: "+print_decoder_cmd)
-			exec(print_decoder_cmd)
+			##exec(print_decoder_cmd)
 
 			#create Autoencoder_hier_X
 			HIER_AE_name_now = "autoencoder_%d_hier_%d" % (latent_dim_en_i, n_AE)
 			if i_AE == 0:
-				init_HIER_AE_cmd = "Autoencoder_hier_%d(%s, %s)" % (encoder_name, decoder_name) 
+				init_HIER_AE_cmd = "Autoencoder_hier_%d(%s, %s)" % (latent_dim_en_i, encoder_name, decoder_name) 
 			elif i_AE > 0:
 				init_HIER_AE_cmd = "Autoencoder_hier_%d(%s, %s, %s)" % \
-												(encoder_name, decoder_name, HIER_AE_name_prev)
+												(latent_dim_en_i, encoder_name, decoder_name, HIER_AE_name_prev)
 			
 			create_HIER_AE_cmd = "%s = %s" % (HIER_AE_name_now, init_HIER_AE_cmd)
 			print("Executing: " + create_HIER_AE_cmd)	
-			exec(create_HIER_AE_cmd)
+			##exec(create_HIER_AE_cmd)
 			compile_HIER_AE_cmd = "%s.compile(optimizer='adam', loss=losses.MeanSquaredError())" \
 										% HIER_AE_name_now
 			print("Executing: " + compile_HIER_AE_cmd)	
-			exec(comple_HIER_AE_cmd)
+			##exec(comple_HIER_AE_cmd)
 			#steps to do
 			logdir = 'autoencoder_%d_hier_%d/' % (latent_dim_en_i, n_AE) + datetime.now().strftime('%Y%m%d-%H%M%S')
 			tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
@@ -121,21 +121,21 @@ class AWWSM4_HIER_AE:
 			arguments_cmd_p3 = "validation_data=(self.X_val, self.X_val)"
 			arguments_cmd_p4 = "callbacks=[tensorboard_callback, model_checkpoint_callback])"
             
-			training_history_cmd_full = "%s = %s" 
-						%(training_history_cmd, fit_cmd+arguments_cmd_p1+arguments_cmd_p2\
+			training_history_cmd_full = "%s = %s" % \
+										(training_history_cmd, fit_cmd+arguments_cmd_p1+arguments_cmd_p2\
 													+arguments_cmd_p3+arguments_cmd_p4) 
 			print("Executing: " + training_history_cmd_full)
-			exec(training_history_cmd_full)
+			##exec(training_history_cmd_full)
 
 			#test section
 			if i_AE > 0:
 				test_prev_cmd = "%s.evaluate(self.X_test, self.X_test)" % HIER_AE_name_prev
 				print("Executing: " + test_prev_cmd)
-				exec(test_prev_cmd)	
+				##exec(test_prev_cmd)	
 				
 			test_now_cmd = "%s.evaluate(self.X_test, self.X_test)" % HIER_AE_name_now
 			print("Executing: " + test_now_cmd)
-			exec(test_now_cmd)	
+			##exec(test_now_cmd)	
 			#end of this round: update the prev HIER
 			print("The %dth round of AE done! Update the prev HIER to be the current HIER!" % n_AE)
 			HIER_AE_name_prev = HIER_AE_name_now
